ba30ae99c905796be417b6a160dea39c4d74dc98
Fabien Lassagne <flassagne@unowhy.com>
2018-01-17 16:08:19 +0100

    chore(rapidjson): configure rapidjson package

---
+++ id.cmake
@@ -0,0 +1,16 @@
+set(package rapidjson)
+set(version 0.1.0)
+
+
+byd__package__info(${package}
+	MAINTAINER_NAME "Fabien Lassagne"
+	MAINTAINER_EMAIL "flassagne@unowhy.com"
+	VERSION ${version}
+	ABI ${version}
+	)
+
+
+byd__package__download_info(${package}
+	URL "https://github.com/Tencent/rapidjson/archive/v1.1.0.tar.gz"
+	URL_HASH SHA1=a3e0d043ad3c2d7638ffefa3beb30a77c71c869f
+	)
